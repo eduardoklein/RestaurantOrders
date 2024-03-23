8d737c3b224e5c06f28d20bff75e04e4d61c313c
Eduardo Klein <eduardo.klein@live.com>
2024-03-23 18:55:49 -0300

    Requisito 2

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,33 @@
+from models.ingredient import Ingredient
 from src.models.dish import Dish  # noqa: F401, E261, E501
+import pytest
 
 
 # Req 2
 def test_dish():
-    pass
+    dish_igual1 = Dish("Cocada", 2.00)
+    dish_igual2 = Dish("Cocada", 2.00)
+    dish_diferente3 = Dish("Feijoada", 12.00)
+
+    assert dish_igual1.name == "Cocada"
+
+    assert dish_igual1 == dish_igual2
+    assert dish_igual1 != dish_diferente3
+
+    assert repr(dish_igual1) == "Dish('Cocada', R$2.00)"
+
+    assert hash(dish_igual1) == hash(dish_igual2)
+    assert hash(dish_igual1) != hash(dish_diferente3)
+
+    sugar = Ingredient("açucar")
+    dish_igual1.add_ingredient_dependency(sugar, 1)
+    assert dish_igual1.recipe == {sugar: 1}
+
+    assert dish_diferente3.get_restrictions() == set()
+    assert dish_diferente3.get_ingredients() == set()
+
+    with pytest.raises(TypeError):
+        Dish("Dish", "preço")
+
+    with pytest.raises(ValueError):
+        Dish("Dish", 0)
--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -9,11 +9,16 @@ def test_ingredient():
     ingredient1 = Ingredient("queijo mussarela")
     ingredient2 = Ingredient("queijo mussarela")
     ingredient3 = Ingredient("manteiga")
+
     assert ingredient1.name == "queijo mussarela"
+
     assert hash(ingredient1) == hash(ingredient2)
     assert hash(ingredient1) != hash(ingredient3)
+
     assert repr(ingredient3) == "Ingredient('manteiga')"
+
     assert ingredient1 == ingredient2
+
     assert ingredient1.restrictions == {
         Restriction.LACTOSE,
         Restriction.ANIMAL_DERIVED,

433b552c40840468dc217f4cdc5094e350c875a7
Eduardo Klein <eduardo.klein@live.com>
2024-03-23 19:19:27 -0300

    Requisito 3

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,24 @@
-# Req 3
+import csv
+from models.dish import Dish
+from models.ingredient import Ingredient
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = self.make_menu(source_path)
+
+    def make_menu(self, source_path: str):
+        dishes = {}
+        with open(source_path, encoding="utf8") as file:
+            reader = csv.DictReader(file)
+
+            for row in reader:
+                dish_name = row["dish"]
+                if dish_name not in dishes:
+                    dishes[dish_name] = Dish(dish_name, float(row["price"]))
+
+                dishes[dish_name].add_ingredient_dependency(
+                    Ingredient(row["ingredient"]), int(row["recipe_amount"])
+                )
+
+        return dishes.values()

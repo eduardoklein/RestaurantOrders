9dbe10dce5f7e1b7bd8eb314e84c7db7c1725b51
Eduardo Klein <eduardo.klein@live.com>
2024-03-23 15:27:22 -0300

    Requisito 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,20 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+    Restriction,
+)
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient1 = Ingredient("queijo mussarela")
+    ingredient2 = Ingredient("queijo mussarela")
+    ingredient3 = Ingredient("manteiga")
+    assert ingredient1.name == "queijo mussarela"
+    assert hash(ingredient1) == hash(ingredient2)
+    assert hash(ingredient1) != hash(ingredient3)
+    assert repr(ingredient3) == "Ingredient('manteiga')"
+    assert ingredient1 == ingredient2
+    assert ingredient1.restrictions == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED,
+    }
